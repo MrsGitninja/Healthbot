a4b8e7fb249d4b69c461e9238f4fddb0fd4337f8
MrsGitninja <aouda.mathlouthi@gmail.com>
2025-02-14 14:41:53 +0100

    Update similarity5.py

--- similarity5.py
+++ similarity5.py
@@ -64,7 +64,7 @@ def predict_answer(user_input):
     
    # translatuser = GoogleTranslator (to_lang = lang )
     language = GoogleTranslator(source="en", target=lang).translate("You are speaking in: ")
-    st.write(f"{language} {lang}")
+    st.write(f"{language}")
              
 
   
